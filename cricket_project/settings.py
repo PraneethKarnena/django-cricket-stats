4983bbb3a13fc11706d96602825cbced0a434a26
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-13 13:37:42 +0530

    configured app

--- cricket_project/settings.py
+++ cricket_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'stats.apps.StatsConfig', # register the app
 ]
 
 MIDDLEWARE = [
