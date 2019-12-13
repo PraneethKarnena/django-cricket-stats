d193b24f8b91a4592a6c705c18d75e77b66ce639
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-13 13:41:39 +0530

    added Team model

--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class TeamModel(models.Model):
+    """
+    Database table for Team.
+    Contains Team details.
+    """
+
+    name = models.CharField(max_length=255, null=False, blank=False)
+    logo = models.ImageField(null=False, blank=False)
+    club_state = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name} - {self.club_state}'
