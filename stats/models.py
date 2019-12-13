ec16b54da15083ff516021ee98bc2b5d080d652a
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-13 18:04:19 +0530

    created Match model

--- stats/models.py
+++ stats/models.py
@@ -39,3 +39,23 @@ class PlayerModel(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name} - {self.team.name}'
 
+
+class MatchModel(models.Model):
+    """
+    Contains details of the match.
+    And whose the winner
+    """
+
+    teams = models.ManyToManyField(TeamModel, blank=False, related_name='matches')
+
+    STATUS_CHOICES = (
+        ('NTP', 'Not Played'),
+        ('CAN', 'Cancelled'),
+        ('PLD', 'Played')
+    )
+    status = models.CharField(max_length=3, null=False, blank=False, default='NTP')
+    winner = models.ForeignKey(TeamModel, on_delete=models.CASCADE, null=False, blank=False)
+    time = models.DateTimeField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.teams} - {self.status} - {self.time}'
