1a804c845fd511ef5ec63f0a6833cb46c49f1ee0
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-13 13:52:03 +0530

    added unique on Team name

--- stats/models.py
+++ stats/models.py
@@ -7,9 +7,35 @@ class TeamModel(models.Model):
     Contains Team details.
     """
 
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
     logo = models.ImageField(null=False, blank=False)
     club_state = models.CharField(max_length=255, null=False, blank=False)
 
     def __str__(self):
         return f'{self.name} - {self.club_state}'
+
+
+# class PlayerModel(models.Model):
+#     """
+#     Database table for info of the player.
+#     Contains individual stats and details of the player.
+#     """
+
+#     first_name = models.CharField(max_length=255, null=False, blank=False)
+#     last_name = models.CharField(max_length=255, null=False, blank=False)
+
+#     image = models.ImageField(null=False, blank=False)
+#     jersey_num = models.PositiveSmallIntegerField(null=False, blank=False)
+#     country = models.CharField(max_length=255, null=False, blank=False)
+
+#     matches = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+#     runs = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+#     highest_score = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+#     fifties = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+#     hundreds = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+
+#     team = models.ForeignKey(TeamModel, on_delete=models.CASCADE, null=False, blank=False)
+
+#     def __str__(self):
+#         return f''
+
