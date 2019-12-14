6b3ef028f0829e87448e58b0050a636ab80440b5
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-14 22:08:48 +0530

    implemented Player stats

--- stats/urls.py
+++ stats/urls.py
@@ -6,4 +6,5 @@ from stats import views
 urlpatterns = [
     path('', views.home_view),
     path('team-players/<int:team_id>/', views.team_players_view),
+    path('player-stats/<int:player_id>/', views.player_stats_view),
 ]
--- stats/views.py
+++ stats/views.py
@@ -18,3 +18,13 @@ def team_players_view(request, team_id):
     """
     players = models.PlayerModel.objects.filter(team_id=team_id)
     return render(request, 'stats/team_players.html', {'players': players})
+
+
+def player_stats_view(request, player_id=None):
+    """
+    Stats of each player
+    """
+
+    player = models.PlayerModel.objects.get(id=player_id)
+    return render(request, 'stats/player_stats.html', {'player': player})
+
